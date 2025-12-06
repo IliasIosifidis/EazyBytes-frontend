c03336ae6ec50b468ae300dcf0f3f6a14428c0db
ilias iosifidis <iosifidis90@gmail.com>
2025-12-06 22:31:27 +0200

    adding categories and search

--- e-com/src/Views/TextureList.vue
+++ e-com/src/Views/TextureList.vue
@@ -16,9 +16,9 @@ onMounted(() =>{
 <template>
 <div class="p-2 mt-4">
   <h1 class="text-4xl my-5 font-bold italic text-center"> Find a texture that you like! </h1>
-  <div class="flex bg-white flex-wrap">
+  <div class="flex bg-white flex-wrap gap-1">
     <div class="p-1" v-for="texture in textureStore.paginatedTextures">
-      <article class="border border-gray-500 rounded-xl p-1 w-30 flex flex-col">
+      <article class="border bg-white border-gray-500 rounded-xl p-1 w-30 flex flex-col hover:scale-110 duration-150">
         <img :src="getImageUrl(texture.image)">
         <p class="text-gray-500 text-xs">{{ texture.categories }}</p>
         <p class="text-center line-clamp-2">{{texture.name}}</p>
@@ -28,9 +28,9 @@ onMounted(() =>{
 
   </div>
   <div class="flex items-center justify-center text-center gap-5 text-lg">
-    <button class="cursor-pointer" @click="textureStore.prevPage()">«PrevPage</button>
+    <button class="cursor-pointer" @click="textureStore.prevPage()"><i class="text-sm text-gray-700 fa-solid fa-backward"></i>PrevPage</button>
     <p>{{textureStore.currentPage}} of {{textureStore.totalPages}}</p>
-    <button class="cursor-pointer" @click="textureStore.nextPage">NextPage»</button>
+    <button class="cursor-pointer" @click="textureStore.nextPage">NextPage<i class="text-sm text-gray-700 fa-solid fa-forward"></i></button>
   </div>
 
 </div>
