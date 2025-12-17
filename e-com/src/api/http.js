5536df039b69c1e604e50060b78b16b56cef11f0
ilias iosifidis <iosifidis90@gmail.com>
2025-12-17 16:20:53 +0200

    progress

--- e-com/src/api/http.js
+++ e-com/src/api/http.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: import.meta.env.DEV
-    ? "http://localhost:8080/api/v1"
-    : "https://boringtexturesbackend-production.up.railway.app/api/v1",
+  baseURL: "https://boringtexturesbackend-production.up.railway.app/api/v1",
   timeout: 10000,
   withCredentials: true,
 });
